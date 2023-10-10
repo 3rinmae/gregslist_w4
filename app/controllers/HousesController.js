c90d3af5a3e3f99a0b3148b452f6fefe032d69bf
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-10-10 14:38:02 -0600

    drawing houses from API

--- app/controllers/HousesController.js
+++ app/controllers/HousesController.js
@@ -1,9 +1,34 @@
+import { AppState } from "../AppState.js"
+import { House } from "../models/House.js"
+import { housesService } from "../services/HousesService.js"
+import { Pop } from "../utils/Pop.js"
+import { setHTML } from "../utils/Writer.js"
 
 
 
+function _drawHouses() {
+  console.log('DRAWING HOUSES')
+  const houses = AppState.houses
+  let content = ''
+  houses.forEach(house => content += house.HouseCardTemplate)
+  // setHTML('houseCards', content)
+}
 
 export class HousesController {
   constructor() {
-    console.log('houses controller hooked up')
+    // console.log('houses controller hooked up')
+    this.getHouses()
+    _drawHouses()
+
+    AppState.on('houses', _drawHouses)
+  }
+
+  async getHouses() {
+    try {
+      await housesService.getHouses()
+    } catch (error) {
+      Pop.error(error)
+      console.error(error)
+    }
   }
 }
--- app/services/HousesService.js
+++ app/services/HousesService.js
@@ -1,7 +1,15 @@
-
+import { AppState } from "../AppState.js";
+import { House } from "../models/House.js";
+import { api } from "./AxiosService.js";
 
 class HousesService {
 
+  async getHouses() {
+    const res = await api.get('api/houses')
+    console.log('GOT HOUSE', res.data);
+    const newHouses = res.data.map(housePOJO => new House(housePOJO))
+    AppState.houses = newHouses
+  }
 }
 
 export const housesService = new HousesService()
